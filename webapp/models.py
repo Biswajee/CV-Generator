5892fc208e526b07ddea94396406d1ace75937e5
Biswajee <roy.biswajeet161@gmail.com>
2018-06-15 09:48:46 +0530

    updates and feature addition

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import random
 # Create your models here.
+
+
+def generateId():
+    return random.randint(1,50)*3
+
+class reg_info(models.Model):
+    usr_id = models.IntegerField(null=False, default=generateId(), primary_key=True, unique=True)
+    email = models.TextField(null=False, unique=True)
+    password = models.TextField(null=False)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return (self.usr_id+'\n'+self.date+'\n'+self.email+'\n'+self.password)
