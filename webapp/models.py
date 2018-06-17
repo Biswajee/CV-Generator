490b3f8a29db3bcc90ef238e8c4cb5d05b20b0d9
Biswajee <roy.biswajeet161@gmail.com>
2018-06-17 10:37:31 +0530

    Working on db support

--- webapp/models.py
+++ webapp/models.py
@@ -14,3 +14,31 @@ class reg_info(models.Model):
 
     def __str__(self):
         return (self.usr_id+'\n'+self.date+'\n'+self.email+'\n'+self.password)
+
+class resume_data():
+    # usr_id must be foreign key for reg_info table's usr_id.
+    usr_id = models.IntegerField(null=False, primary_key=True, unique=True)
+
+    name = models.TextField(null=False)
+    position = models.TextField(null=False)
+    univ = models.TextField(null=False)
+    email = models.TextField(null=False, unique=True)
+    session = models.TextField(null=False)
+    ph = models.TextField(null=False)
+    cph = models.TextField(null=False)
+    cemail = models.TextField(null=False)
+    desc = models.TextField(null=False)
+    cgpa = models.TextField(null=False, unique=True)
+    skill_1 = models.TextField()
+    skill_2 = models.TextField()
+    skill_3 = models.TextField()
+    skill_4 = models.TextField()
+    skill_5 = models.TextField()
+    skill_6 = models.TextField()
+    xp_1_title = models.TextField()
+    xp_2_title = models.TextField()
+    xp_3_title = models.TextField()
+    xp_4_title = models.TextField()
+    xp_5_title = models.TextField()
+    xp_6_title = models.TextField()
+    
